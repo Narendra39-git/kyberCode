0442e05c84354fde379e39d0c1454064150bbd9b
Narendra39-git <narendrachawda39@gmail.com>
2025-03-17 15:13:14 +0530

    Update test_kyber.c -- updated the avg time code

--- ref/test/test_kyber.c
+++ ref/test/test_kyber.c
@@ -1,13 +1,13 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>  // Include time.h for timing functions
 #include "../kem.h"
 #include "../randombytes.h"
-#include <time.h>
 
 #define NTESTS 1000
 
-static int test_keys(void)
+int main(void)
 {
     uint8_t pk[CRYPTO_PUBLICKEYBYTES];
     uint8_t sk[CRYPTO_SECRETKEYBYTES];
@@ -16,48 +16,42 @@ static int test_keys(void)
     uint8_t key_b[CRYPTO_BYTES];
 
     clock_t start, end;
-    double keygen_time, enc_time, dec_time;
-
-    // Measure Key Generation Time
-    start = clock();
-    crypto_kem_keypair(pk, sk);
-    end = clock();
-    keygen_time = (double)(end - start) / CLOCKS_PER_SEC;
-
-    // Measure Encapsulation Time
-    start = clock();
-    crypto_kem_enc(ct, key_b, pk);
-    end = clock();
-    enc_time = (double)(end - start) / CLOCKS_PER_SEC;
-
-    // Measure Decapsulation Time
-    start = clock();
-    crypto_kem_dec(key_a, ct, sk);
-    end = clock();
-    dec_time = (double)(end - start) / CLOCKS_PER_SEC;
-
-    if (memcmp(key_a, key_b, CRYPTO_BYTES)) {
-        printf("ERROR keys\n");
-        return 1;
-    }
-
-    printf("KeyGen Time: %f sec | Enc Time: %f sec | Dec Time: %f sec\n", keygen_time, enc_time, dec_time);
-
-    return 0;
-}
-
-int main(void)
-{
-    unsigned int i;
-    int r;
+    double total_keygen_time = 0.0, total_enc_time = 0.0, total_dec_time = 0.0;
 
     printf("Running %d tests...\n", NTESTS);
-    
-    for (i = 0; i < NTESTS; i++) {
-        r = test_keys();
-        if (r) return 1;
+
+    for (unsigned int i = 0; i < NTESTS; i++) {
+        // Measure Key Generation Time
+        start = clock();
+        crypto_kem_keypair(pk, sk);
+        end = clock();
+        total_keygen_time += (double)(end - start) / CLOCKS_PER_SEC;
+
+        // Measure Encapsulation Time
+        start = clock();
+        crypto_kem_enc(ct, key_b, pk);
+        end = clock();
+        total_enc_time += (double)(end - start) / CLOCKS_PER_SEC;
+
+        // Measure Decapsulation Time
+        start = clock();
+        crypto_kem_dec(key_a, ct, sk);
+        end = clock();
+        total_dec_time += (double)(end - start) / CLOCKS_PER_SEC;
+
+        // Verify that both keys match
+        if (memcmp(key_a, key_b, CRYPTO_BYTES)) {
+            printf("ERROR: Keys do not match!\n");
+            return 1;
+        }
     }
 
+    // Print average execution times
+    printf("Average KeyGen Time: %f sec\n", total_keygen_time / NTESTS);
+    printf("Average Encapsulation Time: %f sec\n", total_enc_time / NTESTS);
+    printf("Average Decapsulation Time: %f sec\n", total_dec_time / NTESTS);
+
+    // Print cryptographic parameter sizes
     printf("CRYPTO_SECRETKEYBYTES:  %d\n", CRYPTO_SECRETKEYBYTES);
     printf("CRYPTO_PUBLICKEYBYTES:  %d\n", CRYPTO_PUBLICKEYBYTES);
     printf("CRYPTO_CIPHERTEXTBYTES: %d\n", CRYPTO_CIPHERTEXTBYTES);
