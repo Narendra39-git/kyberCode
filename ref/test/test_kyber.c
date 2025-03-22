1659781640e080667678a0bcd506cc6d6a0d594e
Narendra39-git <narendrachawda39@gmail.com>
2025-03-22 14:43:01 +0530

    Update test_kyber.c  modi to original code

--- ref/test/test_kyber.c
+++ ref/test/test_kyber.c
@@ -1,14 +1,11 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdint.h>
+#include "../kem.h"
+#include "../randombytes.h"
 #include <time.h>
-// #include "crypto_kem.h"
-#include "randombytes.h"
-#include "aes.h" // Include your AES implementation header
 
-#define MESSAGE "IIT Jammu"
-#define MESSAGE_LEN 10 // Length of the message
-#define NTESTS 1 // Number of tests to run
+#define NTESTS 1000
 
 static int test_keys(void)
 {
@@ -17,8 +14,6 @@ static int test_keys(void)
     uint8_t ct[CRYPTO_CIPHERTEXTBYTES];
     uint8_t key_a[CRYPTO_BYTES];
     uint8_t key_b[CRYPTO_BYTES];
-    uint8_t encrypted_message[MESSAGE_LEN];
-    uint8_t decrypted_message[MESSAGE_LEN];
 
     clock_t start, end;
     double keygen_time, enc_time, dec_time;
@@ -35,17 +30,11 @@ static int test_keys(void)
     end = clock();
     enc_time = (double)(end - start) / CLOCKS_PER_SEC;
 
-    // Encrypt the message using AES with the generated key
-    aes_encrypt(key_b, MESSAGE, encrypted_message, MESSAGE_LEN);
-
     // Measure Decapsulation Time
     start = clock();
     crypto_kem_dec(key_a, ct, sk);
     end = clock();
     dec_time = (double)(end - start) / CLOCKS_PER_SEC;
-
-    // Decrypt the message using AES with the decapsulated key
-    aes_decrypt(key_a, encrypted_message, decrypted_message, MESSAGE_LEN);
 
     if (memcmp(key_a, key_b, CRYPTO_BYTES)) {
         printf("ERROR keys\n");
@@ -53,25 +42,25 @@ static int test_keys(void)
     }
 
     printf("KeyGen Time: %f sec | Enc Time: %f sec | Dec Time: %f sec\n", keygen_time, enc_time, dec_time);
-    printf("Original Message: %s\n", MESSAGE);
-    printf("Encrypted Message: ");
-    for (int i = 0; i < MESSAGE_LEN; i++) {
-        printf("%02x", encrypted_message[i]);
-    }
-    printf("\n");
-    printf("Decrypted Message: %s\n", decrypted_message);
 
     return 0;
 }
 
 int main(void)
 {
+    unsigned int i;
     int r;
 
-    printf("Running %d test...\n", NTESTS);
+    printf("Running %d tests...\n", NTESTS);
+    
+    for (i = 0; i < NTESTS; i++) {
+        r = test_keys();
+        if (r) return 1;
+    }
 
-    r = test_keys();
-    if (r) return 1;
+    printf("CRYPTO_SECRETKEYBYTES:  %d\n", CRYPTO_SECRETKEYBYTES);
+    printf("CRYPTO_PUBLICKEYBYTES:  %d\n", CRYPTO_PUBLICKEYBYTES);
+    printf("CRYPTO_CIPHERTEXTBYTES: %d\n", CRYPTO_CIPHERTEXTBYTES);
 
     return 0;
 }
